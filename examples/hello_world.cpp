c8ff4e0aa70318389b85be1edca52ec529508040
Shane Rozen-Levy <srozen01@seas.upenn.edu>
2021-10-31 11:52:53 -0400

    got basic lcm with manual building of messages

--- examples/hello_world.cpp
+++ examples/hello_world.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <lcm/lcm-cpp.hpp>
+#include "bulk_data.hpp"
 using namespace std;
 
 int main()
 {
+  lcm::LCM lcm;
+  if(!lcm.good())
+    return 1;
   cout << "Hello, World!";
   return 0;
 }
