ec049f2d581d755e7fae5bc4eb9d847b595f38f3
Shane Rozen-Levy <srozen01@seas.upenn.edu>
2021-11-18 16:32:10 -0500

    Added publisher for leg gains

---
+++ python_scripts/leg_gain_publisher.py
@@ -0,0 +1,14 @@
+import lcm
+from lcm_types.leg_gain import leg_gain
+
+msg = leg_gain()
+
+# current gains
+msg.k = 3000
+msg.b = 10
+msg.kp = 60
+msg.kd = 0.2
+msg.kv = 25
+
+lc = lcm.LCM()
+lc.publish("leg_gains", msg.encode())
