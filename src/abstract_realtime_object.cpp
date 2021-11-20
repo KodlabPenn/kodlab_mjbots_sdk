f2552503fc52fe633291f91e971375b7736d8202
Shane Rozen-Levy <srozen01@seas.upenn.edu>
2021-11-19 19:01:54 -0500

    Code works

--- src/abstract_realtime_object.cpp
+++ src/abstract_realtime_object.cpp
@@ -15,19 +15,15 @@ void abstract_realtime_object::join() {
 }
 
 void *abstract_realtime_object::static_run(void *abstract_void_ptr) {
-  std::cout<<"foo"<<std::endl;
   abstract_realtime_object* ptr =
       (static_cast<abstract_realtime_object*>(abstract_void_ptr));
   ptr->set_up_cpu_run();
-  std::cout<<"foo"<<std::endl;
   return nullptr;
 }
 
 void abstract_realtime_object::set_up_cpu_run() {
   std::vector<int> cpu = {m_cpu};
   real_time_tools::fix_current_process_to_cpu(cpu, ::getpid());
-
-  std::cout<<this->m_cpu<<std::endl;
   run();
 }
 
@@ -35,12 +31,9 @@ void abstract_realtime_object::start() {
   m_thread.reset(new real_time_tools::RealTimeThread());
   m_thread->parameters_.cpu_dma_latency_ = -1;
   m_thread->parameters_.priority_ = m_realtime_priority;
-  std::cout<<"Startng thread"<<std::endl;
-  //static_run(this);
   m_thread->create_realtime_thread(static_run, this);
-  std::cout<<"huh"<<std::endl;
-
 }
+
 abstract_realtime_object::abstract_realtime_object() {
 
 }
