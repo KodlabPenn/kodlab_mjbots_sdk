c7e4f507367c9d4e11f8bde6523023cbdfefba8f
J. Diego Caporale <jdcap@seas.upenn.edu>
2022-06-14 20:26:56 -0400

    adds velocity plots to live_motor_plotter and minor cleanup

--- python_scripts/live_motor_plotter.py
+++ python_scripts/live_motor_plotter.py
@@ -10,9 +10,12 @@ import collections
 import time
 
 # Constants
+PLIM = np.pi
+VLIM = 10
 TLIM = 0.01
 WINDOWLENGTH = 2000
 PLOTPERIOD = 1000
+TIMEOUT = 500
 
 
 # Queues
@@ -101,21 +104,22 @@ class BlitManager:
 
 def init_plot():
 
-    fig, axs = plt.subplots(6, 1)
+    fig, axs = plt.subplots(6, 2, sharex=True) # Plot two columns
+    axs = np.concatenate((axs[:,0] ,axs[:,1] )) # Make single vector
 
-    axs[0].set_title("Twist Leg Configuration")
-    axs[0].set_ylabel("Leg 0")
-    axs[0].set_ylim(-np.pi,np.pi)
-    axs[1].set_ylabel("Leg 1")
-    axs[1].set_ylim(-np.pi,np.pi)
-    axs[2].set_ylabel("Leg 2")
-    axs[2].set_ylim(-np.pi,np.pi)
-    axs[3].set_ylabel("Leg 3")
-    axs[3].set_ylim(-np.pi,np.pi)
+    axs[0].set_title("Positions")
+    for i in range(4):
+        axs[i].set_ylabel(f'Leg {i}')
+        axs[i].set_ylim(-PLIM, PLIM)
     axs[4].set_ylabel("Spine")
-    axs[4].set_ylim(-np.pi,np.pi)
+    axs[4].set_ylim(-PLIM, PLIM)
     axs[5].set_ylabel("dt")
     axs[5].set_ylim(0, TLIM)
+    
+    axs[6].set_title("Velocities")
+    for i in range(4):
+        axs[6 + i].set_ylim(-VLIM, VLIM) # Leg joint velocities limits
+    axs[10].set_ylim(-VLIM, VLIM)
 
     times = np.array(timestamps)/1e6
     pos = np.array(positions)
@@ -123,11 +127,20 @@ def init_plot():
     mar = np.array(margins)
     tor = np.array(torques)
     dur = np.array(message_duration)
+    # Initialize line list
     lns=[]
+    # Get leg position lines
     for i in range(4):
         lns.extend( axs[i].plot(times, pos[:, 3*i:3*(i+1)], animated=True) )
+    # Get spine positions line
     lns.extend( axs[4].plot(times, pos[:,12], animated=True))
+    # Get dts
     lns.extend( axs[5].plot(times,np.concatenate(([0],np.diff(times))), animated=True))
+    # Get leg velocities lines
+    for i in range(4):
+        lns.extend( axs[6 + i].plot(times, vel[:, 3*i:3*(i+1)], animated=True))
+    # Get spine velocity line
+    lns.extend( axs[10].plot(times, vel[:,12], animated=True))
 
     return fig,axs,lns
 
@@ -149,6 +162,10 @@ def update_plot_lines(lns):
     i += 1
     lns[i].set_ydata(np.concatenate(([0],np.diff(times))))
     
+    i += 1
+    for i, j in zip(range(i, i + 12), range(12)):
+        lns[i].set_ydata(vel[:, j])
+
     min_x = np.min(times)
     max_x = np.max(times)
     for ax in axs:
@@ -189,8 +206,8 @@ if __name__ == '__main__':
         count = 0
         lc.handle()
         while True:
-            if count>20:
-                lc.handle_timeout(500)
+            if count>20: #ignore first few signals
+                lc.handle_timeout(TIMEOUT)
             count += 1
             currTime = int(time.clock_gettime_ns(time.CLOCK_MONOTONIC_RAW)/1e6) #millis
             if currTime - lastTime > PLOTPERIOD:
