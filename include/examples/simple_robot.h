aaf73030a0dda725e79ac869b5b30727fbac224e
J. Diego Caporale <jdcap@seas.upenn.edu>
2022-07-13 17:56:34 -0400

    adds simple robot class for examples

---
+++ include/examples/simple_robot.h
@@ -0,0 +1,20 @@
+/**
+ * @file simple_robot.h
+ * @author J. Diego Caporale
+ * @brief A simple RobotInterface derived class example. Here is where you would implement any state updates,
+ *        behaviors, or control system
+ * @date 2022-07-19
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#pragma once
+
+#include "kodlab_mjbots_sdk/robot_interface.h"
+
+class SimpleRobot : public kodlab::RobotInterface
+{
+    int mode = 0;
+    using kodlab::RobotInterface::RobotInterface;
+};
