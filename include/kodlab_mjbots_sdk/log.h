54dce64959c0c1c7524440d4902f24ed5b8383cb
Ethan J. Musser <ethanmusser1@gmail.com>
2022-08-12 21:52:38 -0400

    Add logging background color support

--- include/kodlab_mjbots_sdk/log.h
+++ include/kodlab_mjbots_sdk/log.h
@@ -51,43 +51,87 @@
 // Console Printing                                                          //
 ///////////////////////////////////////////////////////////////////////////////
 
-// String Formatting
-#define NEWLINE "\n"
+// C Escapes
+#define BELL "\a"  ///< Terminal bell character
+#define BACKSPACE "\b"  ///< Backspace character
+#define TAB "\t"  ///< Horizontal tab character
+#define NEWLINE "\n"  ///< Newline character
+#define RETURN "\r"  ///< Carriage return character
 
 // OStreams
-#define STDERR stderr
-#define STDOUT stdout
+#define STDERR stderr  ///< Standard error stream
+#define STDOUT stdout  ///< Standard output stream
 
-// Colors
-#define CONSOLE_SEQ_ESC "\x1b"
-#define CONSOLE_SEQ_BEG "["
-#define CONSOLE_SEQ_SEP ";"
-#define CONSOLE_SEQ_END "m"
-#define FONT_REG "0"
-#define FONT_BOLD "1"
-#define COLOR_RESET "0"
-#define COLOR_BLACK "30"
-#define COLOR_RED "31"
-#define COLOR_GREEN "32"
-#define COLOR_YELLOW "33"
-#define COLOR_BLUE "34"
-#define COLOR_MAGENTA "35"
-#define COLOR_CYAN "36"
-#define COLOR_WHITE "37"
+// ANSI Escape Sequences
+#define CONSOLE_SEQ_ESC "\x1b"  ///< ANSI escape sequence
+#define CONSOLE_SEQ_BEG "["  ///< ANSI begin sequence
+#define CONSOLE_SEQ_SEP ";"  ///< ANSI separation sequence
+#define CONSOLE_SEQ_END "m"  ///< ANSI end sequence
+
+// ANSI Graphics Modes
+#define FONT_REGULAR "0"  ///< ANSI reset mode sequence
+#define FONT_BOLD "1"  ///< ANSI bold mode sequence
+#define FONT_DIM "2"  ///< ANSI dim/faint mode sequence
+#define FONT_ITALIC "3"  ///< ANSI italic mode sequence
+#define FONT_UNDERLINE "4"  ///< ANSI underline mode sequence
+#define FONT_BLINK "5"  ///< ANSI blinking mode sequence
+#define FONT_INVERSE "7"  ///< ANSI inverse/reverse mode sequence
+#define FONT_HIDDEN "8"  ///< ANSI hidden/invisible mode sequence
+#define FONT_STRIKETHROUGH "9"  ///< ANSI strikethrough mode sequence
+
+// ANSI Colors
+#define COLOR_RESET "0"  ///< ANSI reset color and mode sequence
+#define COLOR_BLACK_FG "30"  ///< ANSI black foreground sequence
+#define COLOR_RED_FG "31"  ///< ANSI red foreground sequence
+#define COLOR_GREEN_FG "32"  ///< ANSI green foreground sequence
+#define COLOR_YELLOW_FG "33"  ///< ANSI yellow foreground sequence
+#define COLOR_BLUE_FG "34"  ///< ANSI blue foreground sequence
+#define COLOR_MAGENTA_FG "35"  ///< ANSI magenta foreground sequence
+#define COLOR_CYAN_FG "36"  ///< ANSI cyan foreground sequence
+#define COLOR_WHITE_FG "37"  ///< ANSI white foreground sequence
+#define COLOR_DEFAULT_FG "39"  ///< ANSI default foreground sequence
+#define COLOR_BLACK_BG "40"  ///< ANSI black background sequence
+#define COLOR_RED_BG "41"  ///< ANSI red background sequence
+#define COLOR_GREEN_BG "42"  ///< ANSI green background sequence
+#define COLOR_YELLOW_BG "43"  ///< ANSI yellow background sequence
+#define COLOR_BLUE_BG "44"  ///< ANSI blue background sequence
+#define COLOR_MAGENTA_BG "45"  ///< ANSI magenta background sequence
+#define COLOR_CYAN_BG "46"  ///< ANSI cyan background sequence
+#define COLOR_WHITE_BG "47"  ///< ANSI white background sequence
+#define COLOR_DEFAULT_BG "49"  ///< ANSI default background sequence
+#define COLOR_BRIGHT_BLACK_FG "90"  ///< ANSI BRIGHT black foreground sequence
+#define COLOR_BRIGHT_RED_FG "91"  ///< ANSI BRIGHT red foreground sequence
+#define COLOR_BRIGHT_GREEN_FG "92"  ///< ANSI BRIGHT green foreground sequence
+#define COLOR_BRIGHT_YELLOW_FG "93"  ///< ANSI BRIGHT yellow foreground sequence
+#define COLOR_BRIGHT_BLUE_FG "94"  ///< ANSI BRIGHT blue foreground sequence
+#define COLOR_BRIGHT_MAGENTA_FG "95"  ///< ANSI BRIGHT magenta foreground sequence
+#define COLOR_BRIGHT_CYAN_FG "96"  ///< ANSI BRIGHT cyan foreground sequence
+#define COLOR_BRIGHT_WHITE_FG "97"  ///< ANSI BRIGHT white foreground sequence
+#define COLOR_BRIGHT_BLACK_BG "100"  ///< ANSI BRIGHT black background sequence
+#define COLOR_BRIGHT_RED_BG "101"  ///< ANSI BRIGHT red background sequence
+#define COLOR_BRIGHT_GREEN_BG "102"  ///< ANSI BRIGHT green background sequence
+#define COLOR_BRIGHT_YELLOW_BG "103"  ///< ANSI BRIGHT yellow background sequence
+#define COLOR_BRIGHT_BLUE_BG "104"  ///< ANSI BRIGHT blue background sequence
+#define COLOR_BRIGHT_MAGENTA_BG "105"  ///< ANSI BRIGHT magenta background sequence
+#define COLOR_BRIGHT_CYAN_BG "106"  ///< ANSI BRIGHT cyan background sequence
+#define COLOR_BRIGHT_WHITE_BG "107"  ///< ANSI BRIGHT white background sequence
 
 // Color Convenience
 #define FMT_TEXT(font, color) \
   CONSOLE_SEQ_ESC CONSOLE_SEQ_BEG font CONSOLE_SEQ_SEP color CONSOLE_SEQ_END
-#define RESET_COLOR FMT_TEXT(FONT_REG, COLOR_RESET)
+#define FMT_TEXT_BG(font, fg_color, bg_color) \
+  CONSOLE_SEQ_ESC CONSOLE_SEQ_BEG font CONSOLE_SEQ_SEP bg_color CONSOLE_SEQ_SEP fg_color CONSOLE_SEQ_END
+#define RESET_COLOR FMT_TEXT(FONT_REGULAR, COLOR_RESET)
 
 // Console printing via ostreams
-#define PRINT(ostream, ...) std::fprintf(ostream, __VA_ARGS__)
+#define PRINT(ostream, str, args...) std::fprintf(ostream, str NEWLINE, ##args)
 
 // Color printing
 #ifndef NO_COLOR
-#define PRINT_COLOR(ostream, color, str, ...) std::fprintf(ostream, str, __VA_ARGS__)
+#define PRINT_FMT(ostream, ansi_fmt, str, args...) \
+  std::fprintf(ostream, ansi_fmt str RESET_COLOR NEWLINE, ##args)
 #else
-#define PRINT_COLOR(ostream, color, str, ...) PRINT(ostream, __VA_ARGS__)
+#define PRINT_FMT(ostream, color, str, args...) PRINT(ostream, ##args)
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -115,13 +159,13 @@
 #define TAG_FATAL "FATAL"
 
 // Log Severity Tags
-#define COLOR_TRACE FMT_TEXT(FONT_REG, COLOR_MAGENTA)
-#define COLOR_DEBUG FMT_TEXT(FONT_REG, COLOR_BLUE)
-#define COLOR_INFO FMT_TEXT(FONT_REG, COLOR_WHITE)
-#define COLOR_NOTICE FMT_TEXT(FONT_REG, COLOR_GREEN)
-#define COLOR_WARN FMT_TEXT(FONT_REG, COLOR_YELLOW)
-#define COLOR_ERROR FMT_TEXT(FONT_REG, COLOR_RED)
-#define COLOR_FATAL FMT_TEXT(FONT_BOLD, COLOR_RED)
+#define COLOR_TRACE FMT_TEXT(FONT_REGULAR, COLOR_MAGENTA_FG)
+#define COLOR_DEBUG FMT_TEXT(FONT_REGULAR, COLOR_BLUE_FG)
+#define COLOR_INFO FMT_TEXT(FONT_REGULAR, COLOR_WHITE_FG)
+#define COLOR_NOTICE FMT_TEXT(FONT_REGULAR, COLOR_GREEN_FG)
+#define COLOR_WARN FMT_TEXT(FONT_REGULAR, COLOR_YELLOW_FG)
+#define COLOR_ERROR FMT_TEXT(FONT_REGULAR, COLOR_RED_FG)
+#define COLOR_FATAL FMT_TEXT(FONT_BOLD, COLOR_RED_FG)
 
 // Log arguments & formatting flags
 // TODO(ethanmusser): Add time to log output.
@@ -147,19 +191,19 @@
 
 // Log to console
 // TODO(ethanmusser): Implement logging to file.
-#define LOG(msg, tag, args...) PRINT(LOG_OSTREAM, LOG_FORMAT msg NEWLINE, LOG_ARGS(tag), ##args)
-#define VLOG(msg, tag, args...) PRINT(LOG_OSTREAM, VLOG_FORMAT msg NEWLINE, VLOG_ARGS(tag), ##args)
+#define LOG(msg, tag, args...) PRINT(LOG_OSTREAM, LOG_FORMAT msg, LOG_ARGS(tag), ##args)
+#define VLOG(msg, tag, args...) PRINT(LOG_OSTREAM, VLOG_FORMAT msg, VLOG_ARGS(tag), ##args)
 #ifndef NO_COLOR
-#define LOG_COLOR(msg, tag, color, args...) PRINT(LOG_OSTREAM, color LOG_FORMAT msg NEWLINE RESET_COLOR, LOG_ARGS(tag), ##args)
-#define VLOG_COLOR(msg, tag, color, args...) PRINT(LOG_OSTREAM, color VLOG_FORMAT msg NEWLINE RESET_COLOR, VLOG_ARGS(tag), ##args)
+#define LOG_COLOR(msg, tag, color, args...) PRINT_FMT(LOG_OSTREAM, color, LOG_FORMAT msg RESET_COLOR, LOG_ARGS(tag), ##args)
+#define VLOG_COLOR(msg, tag, color, args...) PRINT_FMT(LOG_OSTREAM, color, VLOG_FORMAT msg RESET_COLOR, VLOG_ARGS(tag), ##args)
 #else
 #define LOG_COLOR(msg, tag, color, args...) LOG(msg, tag, args)
 #define VLOG_COLOR(msg, tag, color, args...) VLOG(msg, tag, args)
 #endif
 
-// Minimum log severity (default TRACE)
+// Minimum log severity (default ALL)
 #ifndef LOG_MIN_SEVERITY
-#define LOG_MIN_SEVERITY SEVERITY_TRACE
+#define LOG_MIN_SEVERITY SEVERITY_ALL
 #endif
 
 // Trace logging
