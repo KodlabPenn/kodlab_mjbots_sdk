8a5757acbe5c8dfe283ee9e44a4d98d6cae39c55
Ethan J. Musser <ethanmusser1@gmail.com>
2022-08-21 14:43:00 -0400

    Add TRACE and NOTICE log severities
    The current log severities do not allow for adequate emphasis of information without warning the user.  NOTICE level is provided to fill this void.  Additionally, TRACE has been added as a level belov DEBUG.

--- include/kodlab_mjbots_sdk/log.h
+++ include/kodlab_mjbots_sdk/log.h
@@ -95,32 +95,42 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 // Log Severity Levels
-#define SEVERITY_DEBUG 0
-#define SEVERITY_INFO 1
-#define SEVERITY_WARN 2
-#define SEVERITY_ERROR 3
-#define SEVERITY_FATAL 4
-#define SEVERITY_NONE 5
+#define SEVERITY_ALL 0
+#define SEVERITY_TRACE 1
+#define SEVERITY_DEBUG 2
+#define SEVERITY_INFO 3
+#define SEVERITY_NOTICE 4
+#define SEVERITY_WARN 5
+#define SEVERITY_ERROR 6
+#define SEVERITY_FATAL 7
+#define SEVERITY_NONE 8
 
 // Log Severity Tags
+#define TAG_TRACE "TRACE"
 #define TAG_DEBUG "DEBUG"
 #define TAG_INFO "INFO"
+#define TAG_NOTICE "NOTICE"
 #define TAG_WARN "WARN"
 #define TAG_ERROR "ERROR"
 #define TAG_FATAL "FATAL"
-#define TAG_NONE "NONE"
 
 // Log Severity Tags
+#define COLOR_TRACE FMT_TEXT(FONT_REG, COLOR_MAGENTA)
 #define COLOR_DEBUG FMT_TEXT(FONT_REG, COLOR_BLUE)
 #define COLOR_INFO FMT_TEXT(FONT_REG, COLOR_WHITE)
+#define COLOR_NOTICE FMT_TEXT(FONT_REG, COLOR_GREEN)
 #define COLOR_WARN FMT_TEXT(FONT_REG, COLOR_YELLOW)
 #define COLOR_ERROR FMT_TEXT(FONT_REG, COLOR_RED)
 #define COLOR_FATAL FMT_TEXT(FONT_BOLD, COLOR_RED)
 
 // Log arguments & formatting flags
 // TODO(ethanmusser): Add time to log output.
+#ifndef LOG_ARGS
 #define LOG_ARGS(tag) tag, __FILE__, __func__, __LINE__
-#define LOG_FORMAT "[%-5s][%-15s | %s:%d] "
+#endif
+#ifndef LOG_FORMAT
+#define LOG_FORMAT "[%-6s][%-15s | %s:%d] "
+#endif
 
 // Default ostream (default STDERR)
 #ifndef LOG_OSTREAM
@@ -138,52 +148,72 @@
 
 // Minimum log severity (default DEBUG)
 #ifndef LOG_MIN_SEVERITY
-#define LOG_MIN_SEVERITY SEVERITY_DEBUG
+#define LOG_MIN_SEVERITY SEVERITY_TRACE
+#endif
+
+// Trace logging
+#if LOG_MIN_SEVERITY <= SEVERITY_TRACE
+#define LOG_TRACE(message, args...) LOG_COLOR(message, TAG_TRACE, COLOR_TRACE, ##args)
+#else
+#define LOG_TRACE(message, args...)
 #endif
 
 // Debug logging
-#if LOG_MIN_SEVERITY <= DEBUG_LEVEL
+#if LOG_MIN_SEVERITY <= SEVERITY_DEBUG
 #define LOG_DEBUG(message, args...) LOG_COLOR(message, TAG_DEBUG, COLOR_DEBUG, ##args)
 #else
 #define LOG_DEBUG(message, args...)
 #endif
 
 // Info logging
-#if LOG_MIN_SEVERITY <= INFO_LEVEL
+#if LOG_MIN_SEVERITY <= SEVERITY_INFO
 #define LOG_INFO(message, args...) LOG_COLOR(message, TAG_INFO, COLOR_INFO, ##args)
 #else
 #define LOG_INFO(message, args...)
 #endif
 
+// Notice logging
+#if LOG_MIN_SEVERITY <= SEVERITY_NOTICE
+#define LOG_NOTICE(message, args...) LOG_COLOR(message, TAG_NOTICE, COLOR_NOTICE, ##args)
+#else
+#define LOG_NOTICE(message, args...)
+#endif
+
 // Warn logging
-#if LOG_MIN_SEVERITY <= WARN_LEVEL
+#if LOG_MIN_SEVERITY <= SEVERITY_WARN
 #define LOG_WARN(message, args...) LOG_COLOR(message, TAG_WARN, COLOR_WARN, ##args)
 #else
 #define LOG_WARN(message, args...)
 #endif
 
 // Error logging
-#if LOG_MIN_SEVERITY <= ERROR_LEVEL
+#if LOG_MIN_SEVERITY <= SEVERITY_ERROR
 #define LOG_ERROR(message, args...) LOG_COLOR(message, TAG_ERROR, COLOR_ERROR, ##args)
 #else
 #define LOG_ERROR(message, args...)
 #endif
 
 // Fatal logging
-#if LOG_MIN_SEVERITY <= FATAL_LEVEL
+#if LOG_MIN_SEVERITY <= SEVERITY_FATAL
 #define LOG_FATAL(message, args...) LOG_COLOR(message, TAG_FATAL, COLOR_FATAL, ##args)
 #else
 #define LOG_FATAL(message, args...)
 #endif
 
 // Conditional logs (always defined)
-#if LOG_MIN_SEVERITY <= NONE_LEVEL
+#if LOG_MIN_SEVERITY <= SEVERITY_NONE
+#define LOG_IF_TRACE(condition, message, args...) \
+    if (condition)                                \
+    LOG_COLOR(message, TAG_TRACE, COLOR_TRACE, ##args)
 #define LOG_IF_DEBUG(condition, message, args...) \
     if (condition)                                \
     LOG_COLOR(message, TAG_DEBUG, COLOR_DEBUG, ##args)
 #define LOG_IF_INFO(condition, message, args...) \
     if (condition)                               \
     LOG_COLOR(message, TAG_INFO, COLOR_INFO, ##args)
+#define LOG_IF_NOTICE(condition, message, args...) \
+    if (condition)                                \
+    LOG_COLOR(message, TAG_NOTICE, COLOR_NOTICE, ##args)
 #define LOG_IF_WARN(condition, message, args...) \
     if (condition)                               \
     LOG_COLOR(message, TAG_WARN, COLOR_WARN, ##args)
@@ -194,8 +224,10 @@
     if (condition)                                \
     LOG_COLOR(message, TAG_FATAL, COLOR_FATAL, ##args)
 #else
+#define LOG_IF_TRACE(condition, message, args...)
+#define LOG_IF_DEBUG(condition, message, args...)
 #define LOG_IF_INFO(condition, message, args...)
-#define LOG_IF_INFO(condition, message, args...)
+#define LOG_IF_NOTICE(condition, message, args...)
 #define LOG_IF_WARN(condition, message, args...)
 #define LOG_IF_ERROR(condition, message, args...)
 #define LOG_IF_FATAL(condition, message, args...)
