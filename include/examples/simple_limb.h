ee976a8475e141d14f614ef6e530632e0b853266
zacgong <zacgong@seas.upenn.edu>
2022-09-02 11:14:13 -0400

    implement FK function for SimpleLimb

--- include/examples/simple_limb.h
+++ include/examples/simple_limb.h
@@ -11,5 +11,34 @@
 
 #pragma once
 
+#include <Eigen/Dense>
 #include "kodlab_mjbots_sdk/limb_base.h"
 
+/**
+ * @brief A simple 1DOF limb, just 1 joint with an end effector with a specified
+ * xyz offset, shows implementation of kinematics
+ * 
+ */
+
+class SimpleLimb : public kodlab::LimbBase {
+
+    using kodlab::LimbBase::LimbBase;
+
+    public: 
+        void ForwardKinematics() override {
+            Eigen::Matrix4f FK;
+            float theta = positions_[0];
+            FK(0,0) = cos(theta);
+            FK(0,1) = -sin(theta);
+            FK(1,0) = sin(theta);
+            FK(1,1) = cos(theta);
+            FK(2,2) = 1.0;
+            FK(3,3) = 1.0;
+            FK(0,3) = config_.actuator_offsets[0][0] * cos(theta);
+            FK(1,3) = config_.actuator_offsets[0][1] * sin(theta); //DOUBLE CHECK THESE
+            FK(2,3) = config_.actuator_offsets[0][2];
+
+            //return FK;
+        }
+
+};
