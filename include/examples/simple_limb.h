a6405aef6b3e9511afd7ed22ae6a74f9a8f99849
zacgong <zacgong@seas.upenn.edu>
2022-09-02 10:40:36 -0400

    created simple_limb example file

---
+++ include/examples/simple_limb.h
@@ -0,0 +1,15 @@
+/**
+ * @file simple_limb.h
+ * @author Zac Gong
+ * @brief A simple LimbBase derived class example, a 1DOF limb with 1 joint at 
+ *        the origin and a end effector
+ * @date 2022-09-02
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#pragma once
+
+#include "kodlab_mjbots_sdk/limb_base.h"
+
