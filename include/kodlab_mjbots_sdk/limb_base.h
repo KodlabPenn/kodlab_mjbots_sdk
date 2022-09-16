77fcb12859634000eeb923c491b12263fcac74d9
zacgong <zacgong@seas.upenn.edu>
2022-09-16 15:17:07 -0400

    added LimbConfig struct for limbs

--- include/kodlab_mjbots_sdk/limb_base.h
+++ include/kodlab_mjbots_sdk/limb_base.h
@@ -20,3 +20,30 @@
 #include <Eigen/Dense>
 
 #include "kodlab_mjbots_sdk/joint_base.h"
+
+namespace kodlab
+{
+    /**
+     * @brief struct containing structural information to a limb
+     * @details all information provided by user at initialization
+     */
+    struct LimbConfig {
+        /**
+         * @brief Offset between ceneters of two joints, in the frame of the 
+         * first joint
+         */
+        std::vector<std::array<float, 3>> actuator_offsets; 
+
+        /**
+         * @brief Rotation matrix transforming the previous joint frame to 
+         * the current joint frame, beginning with \f${}^0 R_1\f$
+         */
+        std::vector<Eigen::Matrix3f> actuator_orientations; 
+
+        /**
+         * @brief Matrix of zeros and ones for reordering joints in member 
+         * methods 
+         */
+        Eigen::MatrixXf selection_matrix;
+    };
+}
