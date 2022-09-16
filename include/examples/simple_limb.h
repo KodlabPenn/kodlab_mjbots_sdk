be28213e6aaa9f385bb0170c8f6d95e9952cddb4
zacgong <zacgong@seas.upenn.edu>
2022-09-16 15:49:54 -0400

    added copyright and include files to simple_limb

---
+++ include/examples/simple_limb.h
@@ -0,0 +1,16 @@
+/**
+ * @file simple_limb.h
+ * @author Zac Gong
+ * @brief A simple LimbBase derived class example, a 1DOF limb with 1 joint at
+ *        the origin and a end effector
+ * @date 2022-09-02
+ *
+ * @copyright (c) Copyright 2022 The Trustees of the University of Pennsylvania
+ *
+ */
+
+#pragma once
+
+#include <Eigen/Dense>
+
+#include "kodlab_mjbots_sdk/limb_base.h"
