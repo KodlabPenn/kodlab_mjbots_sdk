38d4cee884ed96fa261081784af67e7cf5d96cb5
zacgong <zacgong@seas.upenn.edu>
2022-09-16 15:33:45 -0400

    added copyright and include files

---
+++ src/limb_base.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file limb_base.cpp
+ * @author Kodlab - Zac Gong (zacgong@seas.upenn.edu)
+ * @brief Class implementation for LimbBase.
+ * @date 2022-08-18
+ *
+ * @copyright BSD 3-Clause License, Copyright (c) 2021 The Trustees of the University of Pennsylvania. All Rights Reserved
+ *
+ */
+
+#include "kodlab_mjbots_sdk/limb_base.h"
